60ebe8d529eec576e5b11de2892a1b80ad5c86c6
Dana Sunalova <dana@paxet.io>
2023-01-03 14:21:36 +0100

    issue-163 - implemented support for v2.0.0 for TcpServer
    - removed support of get/put TcpServerLocalIpv4Address in favor of
    get/put TcpServerLocalAddress
    - implemented get/put TcpServerLocalProtocol
    - implemented get/put TcpServerDescription
    
    Signed-off-by: Dana Sunalova <dana@paxet.io>

--- server/service/TcpServerService.js
+++ server/service/TcpServerService.js
@@ -2,18 +2,18 @@
 var fileOperation = require('onf-core-model-ap/applicationPattern/databaseDriver/JSONDriver');
 
 /**
- * Returns IPv4 address of the server
+ * Returns Description of TcpServer
  *
- * uuid String 
- * returns inline_response_200_19
+ * uuid String
+ * returns inline_response_200_32
  **/
-exports.getTcpServerLocalIpv4Address = function (url) {
-  return new Promise(async function (resolve, reject) {
+exports.getTcpServerDescription = function(url) {
+  return new Promise(async function(resolve, reject) {
     try {
       var value = await fileOperation.readFromDatabaseAsync(url);
       var response = {};
       response['application/json'] = {
-        "tcp-server-interface-1-0:ipv-4-address": value
+        "tcp-server-interface-1-0:description" : value
       };
       if (Object.keys(response).length > 0) {
         resolve(response[Object.keys(response)[0]]);
@@ -26,12 +26,36 @@ exports.getTcpServerLocalIpv4Address = function (url) {
   });
 }
 
+/**
+ * Returns address of the server
+ *
+ * uuid String
+ * returns inline_response_200_34
+ **/
+exports.getTcpServerLocalAddress = function (url) {
+  return new Promise(async function (resolve, reject) {
+    try {
+      var value = await fileOperation.readFromDatabaseAsync(url);
+      var response = {};
+      response['application/json'] = {
+        "tcp-server-interface-1-0:local-address": value
+      };
+      if (Object.keys(response).length > 0) {
+        resolve(response[Object.keys(response)[0]]);
+      } else {
+        resolve();
+      }
+    } catch (error) {
+      reject();
+    }
+  });
+}
 
 /**
  * Returns TCP port of the server
  *
- * uuid String 
- * returns inline_response_200_20
+ * uuid String
+ * returns inline_response_200_35
  **/
 exports.getTcpServerLocalPort = function (url) {
   return new Promise(async function (resolve, reject) {
@@ -52,15 +76,39 @@ exports.getTcpServerLocalPort = function (url) {
   });
 }
 
+/**
+ * Returns Protocol of TcpServer
+ *
+ * uuid String
+ * returns inline_response_200_33
+ **/
+exports.getTcpServerLocalProtocol = function(url) {
+  return new Promise(async function(resolve, reject) {
+    try {
+      var value = await fileOperation.readFromDatabaseAsync(url);
+      var response = {};
+      response['application/json'] = {
+        "tcp-server-interface-1-0:local-protocol" : value
+      };
+      if (Object.keys(response).length > 0) {
+        resolve(response[Object.keys(response)[0]]);
+      } else {
+        resolve();
+      }
+      } catch (error) {
+        reject();
+    }
+  });
+}
 
 /**
- * Documents IPv4 address of the server
+ * Documents Description of TcpServer
  *
- * body Localaddress_ipv4address_body 
- * uuid String 
+ * body Tcpserverinterfaceconfiguration_description_body
+ * uuid String
  * no response value expected for this operation
  **/
-exports.putTcpServerLocalIpv4Address = function (url, body) {
+exports.putTcpServerDescription = function(url, body) {
   return new Promise(async function (resolve, reject) {
     try {
       await fileOperation.writeToDatabaseAsync(url, body, false);
@@ -71,15 +119,50 @@ exports.putTcpServerLocalIpv4Address = function (url, body) {
   });
 }
 
+/**
+ * Documents address of the server
+ *
+ * body Tcpserverinterfaceconfiguration_localaddress_body 
+ * uuid String
+ * no response value expected for this operation
+ **/
+exports.putTcpServerLocalAddress = function (url, body) {
+  return new Promise(async function (resolve, reject) {
+    try {
+      await fileOperation.writeToDatabaseAsync(url, body, false);
+      resolve();
+    } catch (error) {
+      reject();
+    }
+  });
+}
 
 /**
  * Documents TCP port of the server
  *
- * body Tcpserverinterfaceconfiguration_localport_body 
- * uuid String 
+ * body Tcpserverinterfaceconfiguration_localport_body
+ * uuid String
  * no response value expected for this operation
  **/
 exports.putTcpServerLocalPort = function (url, body) {
+  return new Promise(async function (resolve, reject) {
+    try {
+      await fileOperation.writeToDatabaseAsync(url, body, false);
+      resolve();
+    } catch (error) {
+      reject();
+    }
+  });
+}
+
+/**
+ * Documents Protocol of TcpServer
+ *
+ * body Tcpserverinterfaceconfiguration_localprotocol_body
+ * uuid String
+ * no response value expected for this operation
+ **/
+exports.putTcpServerLocalProtocol = function(url, body) {
   return new Promise(async function (resolve, reject) {
     try {
       await fileOperation.writeToDatabaseAsync(url, body, false);
